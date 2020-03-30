45b617852b07a9306d61ed9b1ad4f53fe5c2c6d2
laskar-ksatria <dock_fest@yahoo.co.id>
2020-03-29 18:25:30 -0700

    benerin

--- routes/loghistoryRouter.js
+++ routes/loghistoryRouter.js
@@ -8,15 +8,15 @@ const { logHistory } = require('../controllers/userController')
 const history = require('../models/logHistory');
 
 // create a new accounts
-Router.post("/", authentification, AccountController.addHistory);
-Router.get("/", AccountController.readAll);
-Router.get("/myLogHistory", authentification, function (req,res,next) {
-    let user = req.decoded.id;
-    history.find({user})
-        .then(function (myHistorys) {
-            res.status(200).json(myHistorys)
-        })
-        .catch(next);
-});
+Router.post("/", authentification, logHistory);
+// Router.get("/", AccountController.readAll);
+// Router.get("/myLogHistory", authentification, function (req,res,next) {
+//     let user = req.decoded.id;
+//     history.find({user})
+//         .then(function (myHistorys) {
+//             res.status(200).json(myHistorys)
+//         })
+//         .catch(next);
+// });
 
 module.exports = Router;
